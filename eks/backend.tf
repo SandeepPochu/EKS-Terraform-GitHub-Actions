a37a246b6c4076007353574d0c41d2f00c073497
SandeepPochu <37971705+SandeepPochu@users.noreply.github.com>
2024-12-14 20:57:40 -0700

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1122"
+    bucket         = "mernproj575"
     region         = "us-east-2"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "mernproj575"
     encrypt        = true
   }
 }
